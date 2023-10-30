34ce5128ba7682ebe161c42ea6fc836d14893895
HelloZOOO <qnrwnd10119@gmail.com>
2023-10-30 04:21:09 +0000

    Initial commit

--- git_push_function.sh
+++ git_push_function.sh
@@ -20,14 +20,13 @@ function CLONE_GIT_REPO() {
 
     # Create the repository on GitHub using the PAT
     curl -u "$GITHUB_USERNAME:$GITHUB_TOKEN" https://api.github.com/user/repos -d "{\"name\":\"$REPO_NAME\"}"
-    
-    
+
     # Add, commit, and push the file to the newly created repository
     git init
     touch test.md
     git add .
     git commit -m "Initial commit"
-    git remote add origin https://github.com/$GITHUB_USERNAME/$REPO_NAME.git
+    git remote add origin "https://github.com/$GITHUB_USERNAME/$REPO_NAME.git"
     git remote set-url origin https://$GITHUB_USERNAME@github.com/$GITHUB_USERNAME/$REPO_NAME.git
     git branch -M main
     git push -u origin main
